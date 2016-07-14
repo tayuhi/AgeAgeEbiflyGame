0817d2f9faaf2896d25d0bab09719776b77fc2e4
Yuhi takahashi <mrpt79276945@gmail.com>
2016-07-14 11:57:16 +0900

    飛んでくるものとエビフライの表示

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,8 @@
 var res = {
     background_png : "res/background.png",
-    ship_png : "res/ship.png",
+    ship_png : "res/shrimp03.png",
     particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png",
+    asteroid_png : "res/nagoya0.png",
     bgm_main : "res/bgm_main.mp3",
     se_bang : "res/se_bang.mp3"
 };
